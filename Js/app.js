23bb4a1fcaac9efb145b9b8a485a2e1f46fb6dfd
NicolasEQuintana <nicolasequintana@gmail.com>
2022-02-06 13:59:05 -0300

    secondCommit

--- Js/app.js
+++ Js/app.js
@@ -3,7 +3,9 @@ const menu = document.querySelector('#menu');
 
 function abrirMenu () {
     menu.classList.toggle('show');
+    btnMenu.classList.toggle('hamRotate');
 }
+
 
 btnMenu.addEventListener('click', function () {
     abrirMenu()
