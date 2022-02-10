6aee25afda799b1d038cedaebbfedc75a508d655
NicolasEQuintana <nicolasequintana@gmail.com>
2022-02-10 20:35:33 -0300

    thirdCommit

--- Js/app.js
+++ Js/app.js
@@ -1,5 +1,7 @@
 const btnMenu = document.querySelector('#btnMenu');
 const menu = document.querySelector('#menu');
+const link = document.querySelectorAll('.link')
+const menuOpen = false
 
 function abrirMenu () {
     menu.classList.toggle('show');
@@ -9,4 +11,11 @@ function abrirMenu () {
 
 btnMenu.addEventListener('click', function () {
     abrirMenu()
+     menuOpen=true
+})
+
+link.addEventListener('click', function(){
+    if(menuOpen==true){
+        menu.classList.remove('show')
+    }
 })
